0a55e2b5b49a272271228724b477c5229cfc2bb2
unknown <your.email@example.com>
2024-07-28 10:25:42 +0300

    commited.

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,3 +2,9 @@ import React, { useContext, useEffect } from 'react';
 import { BookContext } from '../context/BookContext';
 import BookItem from './BookItem';
 import { getBooks } from '../services/bookServices';
+const BookList = () => {
+    const { books, dispatch } = useContext(BookContext);
+    return (
+        <h1>welicome book list</h1>
+      );
+}
