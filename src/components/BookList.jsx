e00f0be13497dfbf165ce5ed787698565b9df3b6
unknown <your.email@example.com>
2024-07-28 10:31:53 +0300

    commited.

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -5,6 +5,13 @@ import { getBooks } from '../services/bookServices';
 const BookList = () => {
     const { books, dispatch } = useContext(BookContext);
     return (
-        <h1>welicome book list</h1>
+        <div className="max-w-4xl mx-auto mt-8">
+        <h1 className="text-2xl font-bold mb-4">Book List</h1>
+        <div className="grid grid-cols-1 gap-4">
+          {books.map(book => (
+            <BookItem key={book.id} book={book} />
+          ))}
+        </div>
+      </div>
       );
 }
