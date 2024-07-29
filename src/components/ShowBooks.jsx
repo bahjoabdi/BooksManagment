41445e5386c1e7e1ce3f903f07d3efc64908f64a
unknown <your.email@example.com>
2024-07-29 17:42:36 +0300

    commited.

--- src/components/ShowBooks.jsx
+++ src/components/ShowBooks.jsx
@@ -2,3 +2,17 @@ import React, { useContext, useState } from 'react';
 import { BookContext } from '../context/BookContext';
 import BookItem from '../components/BookItem';
 const ShowBooks = () => {}
+const { books, dispatch } = useContext(BookContext);
+const [searchTerm, setSearchTerm] = useState('');
+
+const filteredBooks = books.filter(
+  (book) =>
+    book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    book.author.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+const handleDelete = (id) => {
+  dispatch({ type: 'DELETE_BOOK', payload: id });
+};
+return (<h1>ShowBook</h1>
+    )
