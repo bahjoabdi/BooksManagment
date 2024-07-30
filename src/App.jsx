ab5d876ff236b8a35359b7390b4795c771b71b83
unknown <your.email@example.com>
2024-07-31 00:46:26 +0300

    Updated routes and deployment configuration

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BookProvider } from './context/BookContext';
 import Home from './pages/Home';
-import  AddBook from './pages/AddBook';
+import AddBook from './pages/AddBook';
 import EditBook from './pages/EditBook';
 import ShowBooks from './components/ShowBooks';
 import About from './pages/About';
