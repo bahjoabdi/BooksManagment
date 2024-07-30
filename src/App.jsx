805b9712b4a29b83bac5fe766451a31cc2df3d8f
unknown <your.email@example.com>
2024-07-31 01:04:10 +0300

    commited.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BookProvider } from './context/BookContext';
 import Home from './pages/Home';
