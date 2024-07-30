fceedabb3dbb4469d77a04261895a5d29f92e203
unknown <your.email@example.com>
2024-07-30 17:15:23 +0300

    commited.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import Home from './pages/Home';
 import AddBook from './pages/AddBook';
 import EditBook from './pages/EditBook';
 import ShowBooks from './components/ShowBooks';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
-    // <BookProvider>
-    //   <Router>
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/add" element={<AddBook />} />
-    //       <Route path="/show" element={<ShowBooks />} />
-    //       <Route path="/edit/:id" element={<EditBook />} />
-    //     </Routes>
-    //   </Router>
-    // </BookProvider>
+    <BookProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/show" element={<ShowBooks />} />
+          <Route path="/edit/:id" element={<EditBook />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </BookProvider>
   );
 };
 
