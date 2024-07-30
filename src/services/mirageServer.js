7675274d9f0d72a30d8d7ca78ef8efdbf8080c0e
unknown <your.email@example.com>
2024-07-30 17:30:40 +0300

    commited.

---
+++ src/services/mirageServer.js
@@ -0,0 +1,44 @@
+// src/mirageServer.js
+import { createServer, Model } from 'miragejs';
+
+export function makeServer({ environment = "development" } = {}) {
+  let server = createServer({
+    environment,
+
+    models: {
+      book: Model,
+    },
+
+    seeds(server) {
+  
+
+    },
+
+    routes() {
+      this.namespace = "api";
+
+      this.get("/books", (schema) => {
+        return schema.books.all();
+      });
+
+      this.post("/books", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody);
+        return schema.books.create(attrs);
+      });
+
+      this.put("/books/:id", (schema, request) => {
+        let newAttrs = JSON.parse(request.requestBody);
+        let id = request.params.id;
+        let book = schema.books.find(id);
+        return book.update(newAttrs);
+      });
+
+      this.del("/books/:id", (schema, request) => {
+        let id = request.params.id;
+        return schema.books.find(id).destroy();
+      });
+    },
+  });
+
+  return server;
+}
