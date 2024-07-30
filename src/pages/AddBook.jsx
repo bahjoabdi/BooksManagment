b752f50da43abcf387abc36f9e14add9e11ce5e8
unknown <your.email@example.com>
2024-07-30 23:02:29 +0300

    commited.

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -106,11 +106,12 @@ const AddBook = () => {
                       onChange={(e) => setPublicationDate(e.target.value)}
                       className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500 sm:text-sm"
                     />
+                    
                   </div>
                   <div>
                     <button
                       type="submit"
-                      className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black bg-blue-400 hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                      className="w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
                     >
                       Add Book
                     </button>
